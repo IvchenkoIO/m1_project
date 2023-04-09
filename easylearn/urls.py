9aa60e059fa28d6fe95a3f5e29b9d789c3e0982e
IvchenkoIO <68115138+IvchenkoIO@users.noreply.github.com>
2023-04-09 14:06:29 +0200

    Add files via upload

---
+++ easylearn/urls.py
@@ -0,0 +1,43 @@
+"""metanit URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from hello import views
+from django.contrib import admin
+from django.views.generic import RedirectView
+from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.index, name='admin'),
+]
+urlpatterns += [
+    path('admin/', admin.site.urls),
+]
+urlpatterns += [
+     path('hello/', include('hello.urls')),
+]
+urlpatterns += [
+     path('files/<str:file_name>/', views.FileDownloader,name='file_dowloader'),
+]
+urlpatterns += [
+    path('', views.index, name='index'),
+]
+
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
